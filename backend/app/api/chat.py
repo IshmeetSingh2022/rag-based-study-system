c0a220265ed8e05c57ec6f74d14985e36a4b207a
IshmeetSingh2022 <145209087+IshmeetSingh2022@users.noreply.github.com>
2026-06-06 12:55:03 +0530

    Syntax error in chats.py route

--- backend/app/api/chat.py
+++ backend/app/api/chat.py
@@ -1,22 +1,21 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 from app.dependencies.db import get_db
 from app.schemas.schemas import ChatRequest, ChatResponse
 from app.services.rag_service import get_rag_response
-from app.middleware.auth import get_current_user
-from fastapi import Request
-router=APIRouter(prefix="chat",tags=["Chat"])
 
-@router.post("/",response_model=ChatResponse)
+router = APIRouter(prefix="/chat", tags=["Chat"])
+
+@router.post("/", response_model=ChatResponse)
 def chat(
     data: ChatRequest,
     db: Session = Depends(get_db),
-    request:Request=None
+    request: Request=None
 ):
-    answer=get_rag_response(
+    answer = get_rag_response(
         question=data.question,
         document_id=data.document_id,
-        user_id=request.state.user_id
+        user_id=request.state.user_id,
         db=db
     )
     return ChatResponse(
