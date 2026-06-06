98eb556e453d83e4fef369c362159dc7ee85c42c
IshmeetSingh2022 <145209087+IshmeetSingh2022@users.noreply.github.com>
2026-06-06 11:34:48 +0530

    added api-document.py to handle upload deletion and retrieval of files

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -11,6 +11,6 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 def signup(data: SignUpRequest, db: Session = Depends(get_db)):
     return signup_user(data, db)
 
-@router.post("/login", response_model=LoginResponse)
+@router.post("/login", response_model=LoginRequest)
 def login(data: LoginRequest, db: Session = Depends(get_db)):
     return login_user(data, db)
---
+++ backend/app/api/documents.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, UploadFile, File, Form
+from sqlalchemy.orm import Session
+from app.dependencies.db import get_db
+from app.schemas.schemas import DocumentResponse
+from fastapi import Request
+from app.services.document_service import(
+    upload_document,
+    get_user_documents,
+    delete_doument
+)
+
+router=APIRouter(prefix="documents",tags=["Documents"])
+
+@router.post("/upload",response_model=DocumentResponse)
+def upload(
+    file:UploadFile=File(...),
+    subject:str=Form(...),
+    db:Session=Depends(get_db),
+    request:Request=None
+):
+    user_id=request.state.user_id
+    return upload_document(file,subject,user_id,db)
+
+@router.get("/",response_model=list[DocumentResponse])
+def get_documents(
+    db:Session=Depends(get_db),
+    request:Request=None
+):  
+    
+    return get_user_documents(request.state.user_id,db)
+
+@router.delete("/{document_id}")
+def delete(
+    document_id:int,
+    db:Session=Depends(get_db),
+    request:Request=None
+):
+     return delete_document(document_id, request.state.user_id, db)
