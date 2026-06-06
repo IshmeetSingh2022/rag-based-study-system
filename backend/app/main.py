76b8f39a8ebdb42e7e6d346644d93260c89efce1
IshmeetSingh2022 <145209087+IshmeetSingh2022@users.noreply.github.com>
2026-06-06 12:59:55 +0530

    update gitignore — add vscode

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,32 +1,35 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from app.core.database import Base, engine
 from app.middleware.auth import auth_middleware
-
 from app.api import auth, documents, chat
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
 
 app = FastAPI(
     title="StudyBuddy AI",
     description="RAG based study assistant",
     version="1.0.0",
+    lifespan=lifespan
 )
-
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://localhost:3000"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.add_middleware(BaseHTTPMiddleware, dispatch=auth_middleware)
-
-
-@app.on_event("startup")
-def create_tables():
-    Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
