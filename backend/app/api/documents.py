d5afb1711f093df5b1973eb2745f03a813cb6ff9
IshmeetSingh2022 <145209087+IshmeetSingh2022@users.noreply.github.com>
2026-06-06 12:56:46 +0530

    Syntax error in documents.py route

--- backend/app/api/documents.py
+++ backend/app/api/documents.py
@@ -1,38 +1,38 @@
-from fastapi import APIRouter, Depends, UploadFile, File, Form
+from fastapi import APIRouter, Depends, UploadFile, File, Form, Request
 from sqlalchemy.orm import Session
 from app.dependencies.db import get_db
 from app.schemas.schemas import DocumentResponse
-from fastapi import Request
-from app.services.document_service import(
+from app.services.document_service import (
     upload_document,
     get_user_documents,
-    delete_doument
+    delete_document
 )
 
-router=APIRouter(prefix="documents",tags=["Documents"])
+router = APIRouter(prefix="/documents", tags=["Documents"])
 
-@router.post("/upload",response_model=DocumentResponse)
+
+@router.post("/upload", response_model=DocumentResponse)
 def upload(
-    file:UploadFile=File(...),
-    subject:str=Form(...),
-    db:Session=Depends(get_db),
-    request:Request=None
+    file: UploadFile = File(...),
+    subject: str = Form(...),
+    db: Session = Depends(get_db),
+    request: Request=None
 ):
-    user_id=request.state.user_id
-    return upload_document(file,subject,user_id,db)
+    return upload_document(file, subject, request.state.user_id, db)
 
-@router.get("/",response_model=list[DocumentResponse])
+
+@router.get("/", response_model=list[DocumentResponse])
 def get_documents(
-    db:Session=Depends(get_db),
-    request:Request=None
-):  
-    
-    return get_user_documents(request.state.user_id,db)
+    db: Session = Depends(get_db),
+    request: Request=None
+):
+    return get_user_documents(request.state.user_id, db)
+
 
 @router.delete("/{document_id}")
 def delete(
-    document_id:int,
-    db:Session=Depends(get_db),
-    request:Request=None
+    document_id: int,
+    db: Session = Depends(get_db),
+    request: Request=None
 ):
-     return delete_document(document_id, request.state.user_id, db)
+    return delete_document(document_id, request.state.user_id, db)
