39ef4b5f16dee079dd5eae5ba7ed504077dd75e4
IshmeetSingh2022 <145209087+IshmeetSingh2022@users.noreply.github.com>
2026-06-12 19:31:57 +0530

    added newr version of Chroma

--- backend/app/services/rag_service.py
+++ backend/app/services/rag_service.py
@@ -5,7 +5,7 @@ from app.models.message import Message
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from app.core.config import settings
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda, RunnableParallel
 
 CHROMA_DIR = "./chroma_db"
