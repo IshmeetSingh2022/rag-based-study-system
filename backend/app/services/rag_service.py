22ab4a59f57cbfeff11c7b33d78dd59fc6435eea
IshmeetSingh2022 <145209087+IshmeetSingh2022@users.noreply.github.com>
2026-06-12 19:13:43 +0530

    improve retriever and prompt

--- backend/app/services/rag_service.py
+++ backend/app/services/rag_service.py
@@ -14,9 +14,9 @@ prompt = ChatPromptTemplate.from_messages([
     (
         "system",
         """You are a helpful study assistant.
-        Answer only based on the context provided below.
-        If the answer is not in the context, say:
-        'Not enough information.'
+        Answer based on the context provided below.
+        If the context has relevant information, use it to answer thoroughly.
+        If the answer is not in the context, say: 'Not enough information.'
         
         Context:
         {context}
@@ -74,12 +74,11 @@ def get_rag_response(question: str, document_id: int, user_id: int, db: Session)
     )
 
     retriever = vector_store.as_retriever(
-        search_type="mmr",
-        search_kwargs={
-            "k": 4,
-            "fetch_k": 10
-        }
-    )
+    search_type="similarity",  
+    search_kwargs={
+        "k": 6,  
+    }
+   )
 
     llm = ChatOpenAI(
         api_key=settings.OPENAI_API_KEY,
